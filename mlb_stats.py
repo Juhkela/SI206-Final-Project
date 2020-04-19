ec2556ee026a4bf4d27772765e422be5064740f6
Jacob Leslie <JacobLeslie@Jakes-MacBook-Pro-2.local>
2020-04-19 16:49:04 -0500

    not much

--- mlb_stats.py
+++ mlb_stats.py
@@ -3,6 +3,7 @@ import re
 import json
 import sqlite3
 import os
+import pprint
 
 
 def setUpDatabase(db_name):
@@ -13,18 +14,27 @@ def setUpDatabase(db_name):
 	cur = conn.cursor()
 	return cur, conn
 
-def getPlayerID(name, cur, conn):
-    """ Take name from database, search in Stats API and return the player id """
+def getPlayerID(name):
+    """ Take name from database, search in Stats API and return the player id.
+    Returns 0 for pitchers so easily filtered out in table """
 
-    url = "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part=" + name + "%25&search_player_all.col_in=player_id&search_player_all.col_in=position"
-    r = requests.get(url)
-    dic = json.loads(r)
-    print(dic)
-    return
+    url = "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='" + name + "%25'&search_player_all.col_in=player_id&search_player_all.col_in=position"
+    r = requests.get(url).json()
+    if r['search_player_all']['queryResults']['row']['position'] == 'P':
+        return 0
+    id = r['search_player_all']['queryResults']['row']['player_id']
     
+    return id
+    
+def createPlayerDict(player_id):
+    """ Takes in a player id and requests the stats from the MLB API.  Returns a dictionary with those values"""
+    pass
 
+def setUpStatsTable(cur, conn):
+    """ Iterates through walkup song table and creates new table with various stats for each player"""
 
-def setUpStatsTable():
+    cur.execute("DROP TABLE IF EXISTS Stats")
+    cur.execute("CREATE TABLE Stats (name TEXT PRIMARY KEY,  avg )
     pass
 
 
@@ -37,6 +47,8 @@ Main program section to test above functions.
 if __name__ == '__main__':
     cur, conn = setUpDatabase('walkup.db')
     getPlayerID("Seth Lugo", cur, conn)
+
+    conn.close()
     
 
 	
