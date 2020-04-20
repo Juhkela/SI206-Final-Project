762d03da8a6d6b77a1140daca15357cd3d2b3555
juhkela <jakela@umich.edu>
2020-04-20 05:00:32 -0400

    Empty Table Created. We are only allowed to use One Database

--- spotify_mlb.py
+++ spotify_mlb.py
@@ -10,24 +10,13 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from json.decoder import JSONDecodeError
 
-# def setUpDatabase(db_name):
-# """ Creates database and returns cur and conn """  ---- > CURRENTLY NOT IN USE. PLANNING TO ADD DB IN LATER
-# 	path = os.path.dirname(os.path.abspath(__file__))
-# 	conn = sqlite3.connect(path + '/' + db_name)
-# 	cur = conn.cursor()
-# 	return cur, conn
-
 
 def fillPlaylist(user_id, playlist_id, cur, conn):
-    token = 'BQA1Y3NVWyaP8BRu5UADYbU5OQGRjAQgdQLqn9Zlites0HzBFXxn7Yw14wPOEkqV3I-t8X2gA5RpoKcMd_Pyihu1FxT1kHz0hxXAfeIy8XfJcha6s5HD9yC-0PldBpZmmuvMV5iYAgpIMzyBA9kuH4yPVTqM14BNdseo6ei4yzM9V3RMSw2my69df5bvTo9KFzvkj78HXD2WYB6mH3AfS44gTncu'
-    # uri_base = 'spotify:track:'
-    one = []
-    two = []
-    three = []
-    four = []
-    five = []
+    # Access token expires every hour: Grab a new one here --> https://developer.spotify.com/console/post-playlist-tracks/ 
+    token = 'BQCmQ0mWTS_v2RlEwo0kWDlZjgMks1Ss9GdkG6FMbTW1R106kA__NUSYJ0wl5tTl2hE-tT315kBtK3Q_F3c_VbrRiKTHy4c-8FtJ6Go_ogNGYlG8S8O4Eqfs6QSmHwxgO1pa1zVi2KPJCCVQOG7g3HZ6yPECckVNlETmAs2FnN-OU4NdZ-XPum_t-Hkem5bI1MsnBu4ur4fmMCS9qkm2DEeI4ja8'
+    one = [], two = [], three = [], four = [], five = [], six = [], last_fifteen = []
     track_ids = []
-    cur.execute('SELECT id FROM Walkup')
+    cur.execute('SELECT id FROM Walkup') # Grabs Spotify Song ID's from the Walkup table
     tuple_id = cur.fetchall()
 
     for i in range(len(tuple_id)):
@@ -42,43 +31,35 @@ def fillPlaylist(user_id, playlist_id, cur, conn):
         four.append(track_ids[i])
     for i in range(400, 500):
         five.append(track_ids[i])
+    for i in range(500, 600):
+        six.append(track_ids[i])
+    for i in range(600, 615):
+        last_fifteen.append(track_ids[i])
     
-    
-
     sp = spotipy.Spotify(auth = token)
 
-    sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, one) # Prevents adding two of the same song
-    sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, two) 
-    sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, three) 
-    sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, four) 
-    sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, five) 
+    all_songs = [one, two, three, four, five, six, last_fifteen] 
+    # Lists all 615 songs that were gathered from the Walkup table in walkup.db
 
-
-    sp.user_playlist_add_tracks(user_id, playlist_id, one) # Adds in tracks.
-    sp.user_playlist_add_tracks(user_id, playlist_id, two) 
-    sp.user_playlist_add_tracks(user_id, playlist_id, three) 
-    sp.user_playlist_add_tracks(user_id, playlist_id, four) 
-    sp.user_playlist_add_tracks(user_id, playlist_id, five) 
+    for i in all_songs:
+        #sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, i)
+        sp.user_playlist_add_tracks(user_id, playlist_id, i)
     
-    return track_ids
 
+def grabTrackData():
+    pass
 
 def setUpSpotifyTable(data, cur, conn):
-    pass
-
-    
-
-def grabSongData():
-    pass
-
+    cur.execute("DROP TABLE IF EXISTS Spotify")
+    cur.execute('''CREATE TABLE Spotify (title TEXT PRIMARY KEY, song_id TEXT, popularity INTEGER, 
+                   duration_ms REAL, explicit BOOL, track_info TEXT)''')
 
 def main():
     playlist_id = '3ExuKCBDeMaelDsAiMKz97'
     user_id = 'a75zp3fq9fm0y8cyb4eki5yb7'
     conn = sqlite3.connect('walkup.db')
     cur = conn.cursor()
-    track_id = fillPlaylist(user_id, playlist_id, cur, conn)
-    print(track_id)
+    # fillPlaylist(user_id, playlist_id, cur, conn) ---> Do not run this command again.
 
     conn.close()
     
