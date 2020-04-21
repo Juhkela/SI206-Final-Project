21b9ccd1343ba2d6391f78704210c21123a9b645
juhkela <jakela@umich.edu>
2020-04-20 23:28:47 -0400

    Finished Database. This code will only run if you have the Auth Token active

--- spotify_mlb.py
+++ spotify_mlb.py
@@ -10,56 +10,84 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from json.decoder import JSONDecodeError
 
+# Grabbing Auth Token:
+# Client ID:     af0da31f178949c58f60415516fb1faa
+# Client Secret: 7803f391738242ebaf499fcd5ac26645
+# GET https://accounts.spotify.com/authorize?client_id=af0da31f178949c58f60415516fb1faa&response_type=code&redirect_uri=https%3A%2F%2Fgoogle.com%2F&scope=playlist-read-private
+# RUN CURL BELOW IN CMD
+# curl -H "Authorization: Basic YWYwZGEzMWYxNzg5NDljNThmNjA0MTU1MTZmYjFmYWE6NzgwM2YzOTE3MzgyNDJlYmFmNDk5ZmNkNWFjMjY2NDU=" -d grant_type=authorization_code -d code=AQDBQlAxPI4Ga97pk8YUqtic1z74AEiTfShjw-0-2__Z7N0apEXypq7MriH2SzvYoeKJuIdfr0SPdIBLKnDLT3dFby3-jNNSZDW5-W98UaAIWrM8Fxiti7Xs5OpfmHtK_sgpmeBPGRMOE3mIY295FKF5v_41BM9Lmdh5gC2ISSqSA7cVXnae4MYpO2AKuCQKZPBSrlhVn6Bkyv-s3VCxYw -d redirect_uri=https%3A%2F%2Fgoogle.com%2F https://accounts.spotify.com/api/token
 
 def fillPlaylist(user_id, playlist_id, cur, conn):
+
+    # username = sys.argv[1] # The username is 'SI206Project'
+
+    # scope = 'playlist-modify-public'
+    # token = util.prompt_for_user_token(username, scope)
+
     # Access token expires every hour: Grab a new one here --> https://developer.spotify.com/console/post-playlist-tracks/ 
-    token = 'BQCmQ0mWTS_v2RlEwo0kWDlZjgMks1Ss9GdkG6FMbTW1R106kA__NUSYJ0wl5tTl2hE-tT315kBtK3Q_F3c_VbrRiKTHy4c-8FtJ6Go_ogNGYlG8S8O4Eqfs6QSmHwxgO1pa1zVi2KPJCCVQOG7g3HZ6yPECckVNlETmAs2FnN-OU4NdZ-XPum_t-Hkem5bI1MsnBu4ur4fmMCS9qkm2DEeI4ja8'
-    one = [], two = [], three = [], four = [], five = [], six = [], last_fifteen = []
+    token = 'BQDMm68XomWdY68OcawVzQ2CKiz64uT0kgVv44It8EBoKvmNhcXvoB382JOn9VrWiC1O4K8ZULte-mzNFmDOxTeasvicWtscxd5X8c4PokbZO_DUGLuTHr-ARF_YEgjIsRg-UWMdquOVa4C3ezBZCdHzNEOkMrXLpVGakjOplxvyiRc2MqJ_ZrTAaXWMB7qx8T5AlF_Dgq_guE1eZMqJSfg0dpzt'
+    increment = 0
     track_ids = []
-    cur.execute('SELECT id FROM Walkup') # Grabs Spotify Song ID's from the Walkup table
+    cur.execute('SELECT DISTINCT id FROM Walkup') # Grabs Spotify Song ID's from the Walkup table
     tuple_id = cur.fetchall()
 
     for i in range(len(tuple_id)):
         track_ids.append(tuple_id[i][0])
-    for i in range(100):
-        one.append(track_ids[i])
-    for i in range(100, 200):
-        two.append(track_ids[i])
-    for i in range(200, 300):
-        three.append(track_ids[i])
-    for i in range(300, 400):
-        four.append(track_ids[i])
-    for i in range(400, 500):
-        five.append(track_ids[i])
-    for i in range(500, 600):
-        six.append(track_ids[i])
-    for i in range(600, 615):
-        last_fifteen.append(track_ids[i])
     
-    sp = spotipy.Spotify(auth = token)
+    # split_list_tracks = chunks(track_ids, 100)
 
-    all_songs = [one, two, three, four, five, six, last_fifteen] 
-    # Lists all 615 songs that were gathered from the Walkup table in walkup.db
+    # if token:
+    #     sp = spotipy.Spotify(auth = token)
+    #     sp.trace = False
 
-    for i in all_songs:
-        #sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, i)
-        sp.user_playlist_add_tracks(user_id, playlist_id, i)
+    #     for i in split_list_tracks:
+    #         increment += 1
+    #         # sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, i)
+    #         # sp.user_playlist_add_tracks(user_id, playlist_id, i)
+
+    # else:
+    #     print("The token has expired")
+
+    return track_ids
     
 
-def grabTrackData():
-    pass
+def grabTrackData(track_ids):
+    token = 'BQBrk7NnHsguCY2NsVuy8KUjm46cNfA6KXJ-YOcwFw5BFhPGi7q_bdT2VdJNLz9bs0_4guBvUtL_cK8l24OypEatHrEPkL3f9JYrI95rv2ATJxkXLSA8S1t0e0Ag0gp5GrazqKwgYMNFSme10zZl2hvoZ8aOiI3WfID7tPe9lPFRc2s'
+    sp = spotipy.Spotify(auth=token)
+    split_list = chunks(track_ids, 50)
+    track_data = []
+    for i in split_list:
+        track_data.extend(sp.tracks(i)['tracks'])
+
+    return track_data
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+    
 
 def setUpSpotifyTable(data, cur, conn):
     cur.execute("DROP TABLE IF EXISTS Spotify")
-    cur.execute('''CREATE TABLE Spotify (title TEXT PRIMARY KEY, song_id TEXT, popularity INTEGER, 
-                   duration_ms REAL, explicit BOOL, track_info TEXT)''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS Spotify (song_id TEXT PRIMARY KEY, name TEXT, popularity INTEGER, 
+                   duration_ms INTEGER, explicit BOOL, track_info TEXT)''')
+    for i in data:
+        cur.execute('''INSERT INTO Spotify (song_id, name, popularity, duration_ms, explicit, track_info)
+                       VALUES (?, ?, ?, ?, ?, ?)''', 
+                       (i['id'], i['name'], i['popularity'], i['duration_ms'], i['explicit'], i['external_urls']['spotify']))
+
+    conn.commit()
 
 def main():
     playlist_id = '3ExuKCBDeMaelDsAiMKz97'
     user_id = 'a75zp3fq9fm0y8cyb4eki5yb7'
     conn = sqlite3.connect('walkup.db')
     cur = conn.cursor()
-    # fillPlaylist(user_id, playlist_id, cur, conn) ---> Do not run this command again.
+    track_ids = fillPlaylist(user_id, playlist_id, cur, conn) # ---> Playlist filled. No need to run again.
+    # print(track_ids)
+
+    track_data = grabTrackData(track_ids)
+    setUpSpotifyTable(track_data, cur, conn)
+    
 
     conn.close()
     
